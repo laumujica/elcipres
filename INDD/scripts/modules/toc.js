4bfda8b9a44ab20ea009b78bb7ed10b04e83fbea
Lau Mujica <103077935+laumujica@users.noreply.github.com>
2026-09-22 09:42:35 -0300

    Add modular InDesign architecture: toc.js

---
+++ INDD/scripts/modules/toc.js
@@ -0,0 +1,13 @@
+// Reserved for the next milestone:
+// generate the table of contents after
+// body pagination is known.
+
+function createTableOfContents() {
+  throw new Error(
+    "TOC generation is not implemented yet."
+  );
+}
+
+module.exports = {
+  createTableOfContents,
+};
