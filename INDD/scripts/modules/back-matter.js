b6f89fd9abd50aa7a1f6ff60d847ff05df1b8c42
Lau Mujica <103077935+laumujica@users.noreply.github.com>
2026-09-22 09:42:38 -0300

    Add modular InDesign architecture: back-matter.js

---
+++ INDD/scripts/modules/back-matter.js
@@ -0,0 +1,15 @@
+// Reserved for back matter:
+// About the author
+// Acknowledgements
+// Editorial note
+// About this edition
+
+function createBackMatter() {
+  throw new Error(
+    "Back matter generation is not implemented yet."
+  );
+}
+
+module.exports = {
+  createBackMatter,
+};
